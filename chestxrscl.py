03ffe5dda73b34c0a20f1985d44acf16e50a2cdf
sarahanjum12 <127437565+sarahanjum12@users.noreply.github.com>
2024-02-05 23:32:52 +0530

    Add files via upload

--- chestxrscl.py
+++ chestxrscl.py
@@ -1,4 +1,5 @@
 import torch
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
@@ -7,88 +8,64 @@ from torchvision.models import resnet18
 import os
 from PIL import Image
 
-# Define model architecture with single perceptron for contrastive head
-class ContrastiveHead(nn.Module):
-    def __init__(self, in_features):
-        super(ContrastiveHead, self).__init__()
-        self.fc = nn.Linear(in_features, 1)  # Single perceptron layer for contrastive head
+def contrastive_head(features, in_features):
+    fc = nn.Linear(in_features, 1)
+    return fc(features)
 
-    def forward(self, x):
-        return self.fc(x)
+def load_custom_dataset(root_dir, transform=None):
+    images = []
+    targets = []
+    classes = os.listdir(root_dir)
 
-# Custom Dataset class
-class CustomDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.classes = os.listdir(root_dir)
-        self.images = []
-        self.targets = []
+    for cls in classes:
+        cls_path = os.path.join(root_dir, cls)
+        if os.path.isdir(cls_path):
+            images_folder = os.path.join(cls_path, 'images')
+            if os.path.isdir(images_folder):
+                cls_images = [os.path.join(images_folder, f) for f in os.listdir(images_folder) if f.endswith('.png')]
+                for img_path in cls_images:
+                    with open(img_path, 'rb') as f:
+                        img = Image.open(f).convert('RGB')
+                    if transform:
+                        img = transform(img)
+                    images.append(img)
+                    targets.append(classes.index(cls))
 
-        for cls in self.classes:
-            cls_path = os.path.join(self.root_dir, cls)
-            if os.path.isdir(cls_path):
-                images_folder = os.path.join(cls_path, 'images')
-                if os.path.isdir(images_folder):
-                    cls_images = [os.path.join(images_folder, f) for f in os.listdir(images_folder) if f.endswith('.png')]
-                    for img_path in cls_images:
-                        self.images.append(img_path)
-                        self.targets.append(self.classes.index(cls))
+    return images, targets
 
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, index):
-        img_path, target = self.images[index], self.targets[index]
-        with open(img_path, 'rb') as f:
-            img = Image.open(f)
-            img = img.convert('RGB')
-        
-        if self.transform:
-            img = self.transform(img)
-
-        return img, target
-
-# Define data transforms
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
 ])
 
-# Load dataset
-dataset = CustomDataset(root_dir='C:\\Users\\hp\\Downloads\\chestxr\\COVID-19_Radiography_Dataset', transform=transform)
-print("Total number of samples in dataset:", len(dataset))
+images, targets = load_custom_dataset('C:\\Users\\hp\\Downloads\\chestxr\\COVID-19_Radiography_Dataset', transform=transform)
 
+
+dataset = list(zip(images, targets))
 train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
 
 print("Number of samples in train dataset:", len(train_dataset))
 print("Number of samples in test dataset:", len(test_dataset))
 
-# Define data loaders
+
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-# Define backbone model (e.g., ResNet18)
-backbone_model = resnet18(pretrained=True)  # Load pre-trained weights
-backbone_model.fc = nn.Identity()  # Replace the fully connected layer with an identity layer
+backbone_model = resnet18(pretrained=True)
+backbone_model.fc = nn.Identity()
 
-# Define contrastive head for supervised contrastive learning
-contrastive_head = ContrastiveHead(in_features=512)  
-# Define optimizer
-optimizer = optim.Adam(contrastive_head.parameters(), lr=0.001)
+optimizer = optim.Adam(backbone_model.parameters(), lr=0.001)
 
-# Training loop
 num_epochs = 1
 for epoch in range(num_epochs):
-    contrastive_head.train()
+    backbone_model.train()
     running_loss = 0.0
     for batch_idx, (inputs, labels) in enumerate(train_loader):
         optimizer.zero_grad()
         features = backbone_model(inputs)
-        outputs = contrastive_head(features)
-        target = labels.unsqueeze(1).float()  # Reshape labels for supervised contrastive learning
+        outputs = contrastive_head(features, in_features=512)
+        target = labels.unsqueeze(1).float()
         loss = nn.BCEWithLogitsLoss()(outputs, target)
         loss.backward()
         optimizer.step()
@@ -98,3 +75,46 @@ for epoch in range(num_epochs):
             running_loss = 0.0
 
 print('Finished Training')
+
+
+
+images, targets = load_custom_dataset('C:\\Users\\hp\\Downloads\\chestxr\\COVID-19_Radiography_Dataset', transform=transform)
+print("Total number of samples in dataset:", len(dataset))
+
+dataset = list(zip(images, targets))
+train_size = int(0.8 * len(dataset))
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
+
+print("Number of samples in train dataset:", len(train_dataset))
+print("Number of samples in test dataset:", len(test_dataset))
+
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+backbone_model = resnet18(pretrained=True)
+backbone_model.fc = nn.Identity()
+
+optimizer = optim.Adam(backbone_model.parameters(), lr=0.001)
+
+num_epochs = 1
+for epoch in range(num_epochs):
+    backbone_model.train()
+    running_loss = 0.0
+    for batch_idx, (inputs, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        features = backbone_model(inputs)
+        outputs = contrastive_head(features, in_features=512)
+        target = labels.unsqueeze(1).float()
+        loss = nn.BCEWithLogitsLoss()(outputs, target)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if batch_idx % 10 == 9:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 10))
+            running_loss = 0.0
+
+print('Finished Training')
+
+
+
